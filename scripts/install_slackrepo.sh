dbd7cb5c2f0502d529408a01a0b25a04c2536f83
Andrew Clemons <andrew.clemons@gmail.com>
2019-06-14 15:58:12 +1200

    Move installing slackrepo to a proper shellscript.

---
+++ scripts/install_slackrepo.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# Copyright 2019 Andrew Clemons, Wellington New Zealand
+# All rights reserved.
+#
+# Redistribution and use of this script, with or without modification, is
+# permitted provided that the following conditions are met:
+#
+# 1. Redistributions of this script must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS" AND ANY EXPRESS OR IMPLIED
+#  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+#  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO
+#  EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+#  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+#  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+#  OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+#  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+#  OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+#  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+set -e
+set -o pipefail
+
+git clone --depth 1 https://github.com/aclemons/slackbuilds.org.git -b 14.2_acl
+
+export TAG=_jenkins
+(
+  cd slackbuilds.org
+
+  cd system/slackrepo
+  . slackrepo.info
+
+  wget $DOWNLOAD
+  sh slackrepo.SlackBuild
+
+  cd ../slackrepo-hints
+  . slackrepo-hints.info
+
+  wget $DOWNLOAD
+  sh slackrepo-hints.SlackBuild
+)
+
+rm -rf slackbuilds.org
+
+installpkg /tmp/slackrepo-*.t?z
+
+rm -rf /tmp/slackrepo*
+rm -rf /tmp/SBo
+
+(
+  cd  /usr/local/bin
+  wget https://raw.githubusercontent.com/aclemons/slackrepo-jenkins/28b9b80ccbc332b54d7f1010207897d91b23bd88/slackrepo_parse.rb
+  chmod +x slackrepo_parse.rb
+)
