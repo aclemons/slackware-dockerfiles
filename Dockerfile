90af01bf469df20209f6e3870fa6de5751e52eb1
Andrew Clemons <andrew.clemons@gmail.com>
2019-07-14 20:03:09 +1200

    Move install all to a proper shell script.

--- Dockerfile
+++ Dockerfile
@@ -3,16 +3,7 @@ FROM $base_image
 
 ARG base_image
 RUN echo "Using base image $base_image"
-RUN if [ "$base_image" = "vbatts/slackware:current" ] || [ "$base_image" = "aclemons/slackware:current_arm_base" ] ; then touch /var/lib/slackpkg/current ; fi
-RUN sed -i 's/^\(WGETFLAGS="\)\(.*\)$/\1--quiet \2/' /etc/slackpkg/slackpkg.conf
-RUN slackpkg -default_answer=yes -batch=on update
-RUN slackpkg -default_answer=yes -batch=on upgrade slackpkg
-RUN slackpkg -default_answer=yes -batch=on update
-RUN if [ "$base_image" = "vbatts/slackware:current" ] ; then slackpkg -default_answer=yes -batch=on install pcre2 libpsl ; fi
-RUN slackpkg -default_answer=yes -batch=on upgrade-all
-RUN slackpkg -default_answer=yes -batch=on install a/* ap/* d/* e/* f/* k/* kde/* l/* n/* t/* tcl/* x/* xap/* xfce/* y/*
-RUN slackpkg -default_answer=yes -batch=on install-new
-RUN slackpkg -default_answer=yes -batch=on upgrade-all
-RUN slackpkg -default_answer=yes -batch=on clean-system
-RUN slackpkg -default_answer=yes -batch=on install rust
-RUN slackpkg -default_answer=yes -batch=on new-config
+
+COPY scripts/install_all.sh /
+RUN bash /install_all.sh
+RUN rm /install_all.sh
---
+++ scripts/install_all.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Copyright 2019 Andrew Clemons, Wellington New Zealand
+# All rights reserved.
+#
+# Redistribution and use of this script, with or without modification, is
+# permitted provided that the following conditions are met:
+#
+# 1. Redistributions of this script must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS" AND ANY EXPRESS OR IMPLIED
+#  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+#  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO
+#  EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+#  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+#  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+#  OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+#  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+#  OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+#  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+set -e
+set -o pipefail
+
+base_image="$1"
+
+if [ "$base_image" = "vbatts/slackware:current" ] || [ "$base_image" = "aclemons/slackware:current_arm_base" ] ; then
+  touch /var/lib/slackpkg/current
+fi
+
+sed -i 's/^\(WGETFLAGS="\)\(.*\)$/\1--quiet \2/' /etc/slackpkg/slackpkg.conf
+
+slackpkg -default_answer=yes -batch=on update
+slackpkg -default_answer=yes -batch=on upgrade slackpkg
+slackpkg -default_answer=yes -batch=on update
+
+if [ "$base_image" = "vbatts/slackware:current" ] ; then
+  slackpkg -default_answer=yes -batch=on install pcre2 libpsl
+fi
+
+slackpkg -default_answer=yes -batch=on upgrade-all
+slackpkg -default_answer=yes -batch=on install a/* ap/* d/* e/* f/* k/* kde/* l/* n/* t/* tcl/* x/* xap/* xfce/* y/*
+slackpkg -default_answer=yes -batch=on install-new
+slackpkg -default_answer=yes -batch=on upgrade-all
+slackpkg -default_answer=yes -batch=on clean-system
+slackpkg -default_answer=yes -batch=on install rust
+slackpkg -default_answer=yes -batch=on new-config
+
+rm -rf /var/cache/packages/*
