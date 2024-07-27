a50419ae79dba3ac21bc89a29d7ea4746e1d062a
Andrew Clemons <andrew.clemons@gmail.com>
2024-07-27 16:26:39 +0900

    Bump copyright years.

--- scripts/build_base_image.sh
+++ scripts/build_base_image.sh
@@ -2,7 +2,7 @@
 
 # MIT License
 
-# Copyright (c) 2019-2023 Andrew Clemons
+# Copyright (c) 2019-2024 Andrew Clemons
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
